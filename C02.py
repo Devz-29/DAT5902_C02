527431b0b620346ea69d7031a0f1541dda8bdc2e
Devz-29 <devarjun.prabhakar@gmail.com>
2024-12-19 14:27:48 +0000

    Exploring of dataset checking types and nulls

--- C02.py
+++ C02.py
@@ -3,5 +3,13 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import scipy as scp
 
-df = pd.read_csv('co2_emission.csv')
+df = pd.read_csv('co2_emission.csv') # load the data
+
+print(df.head())       # Preview the first few rows
+print(df.info())       # Get an overview of columns, data types, and missing values
+print(df.describe())   # Summary statistics for numerical columns
+
+print(df.isnull().sum()) # Check for missing values
+
+print(df.dtypes) # Check types of columns
 
