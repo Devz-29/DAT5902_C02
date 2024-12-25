a90364b427d5abdef019b86324b22e66d8f13cec
Devz-29 <devarjun.prabhakar@gmail.com>
2024-12-25 16:48:40 +0000

    incorrect column name

--- Unit_Test.py
+++ Unit_Test.py
@@ -5,5 +5,5 @@ def test_data_columns():
     # Check if the dataset has the expected columns
     df = pd.read_csv('co2_emission.csv')
     assert 'Year' in df.columns
-    assert 'CO2_emission' in df.columns
+    assert 'Annual CO₂ emissions (tonnes )' in df.columns
 
